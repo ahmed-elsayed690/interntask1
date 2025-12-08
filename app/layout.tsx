fbfa103ac2f9fbffccc39551e35f84bf8145a0cf
ahmed-elsayed690 <ahmdelsayeed404@gmail.com>
2025-12-08 12:05:33 +0200

    Add LanguageProvider to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./component/ThemeContext";
+import { LanguageProvider } from "./component/LanguageContext"; // Add this
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider>
-          {children}
-        </ThemeProvider>
+        <LanguageProvider> {/* Add this wrapper */}
+          <ThemeProvider>
+            {children}
+          </ThemeProvider>
+        </LanguageProvider>
       </body>
     </html>
   );
